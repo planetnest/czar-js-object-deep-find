4dedd619cc89b028fe8d80216d0d98d84057eacd
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2018-06-27 18:16:17 +0100

    Modified test suite to easily accomodate students' code testing

--- index.test.js
+++ index.test.js
@@ -1,25 +1,30 @@
 const find = require('./');
 
-test('simple find', () => {
-  const a = { b: 'baby', c: { d: 'dad', e: { f: ['fish', 'fly'] } } };
-  expect(find(a, 'f')).toBe(a.c.e.f);
-});
+function Suite(file) {
+  const find = require(file);
 
-test('complex find', () => {
-  const a = require('./res/dump.json');
-  const f = find(a, 'fly');
-  console.log(`f is ${f}`);
-  expect(f).toBeDefined();
-});
+  test('simple find', () => {
+    const a = { b: 'baby', c: { d: 'dad', e: { f: ['fish', 'fly'] } } };
+    expect(find(a, 'f')).toBe(a.c.e.f);
+  });
+  
+  test('complex find', () => {
+    const a = require('./res/dump.json');
+    const f = find(a, 'fly');
+    expect(f).toBeDefined();
+  });
+  
+  test('complex find #2', () => {
+    const a = require('./res/generated.json');
+    const f = find(a, 'nam');
+    expect(f).toBeDefined();
+  });
+  
+  test('should be null if not found', () => {
+    const a = { b: 'baby', c: { d: 'dad', e: { f: ['fish', 'fly'] } } };
+    expect(find(a, 'null')).toBeNull();
+  });
+}
 
-test('complex find #2', () => {
-  const a = require('./res/generated.json');
-  const f = find(a, 'nam');
-  console.log(`f is ${f}`);
-  expect(f).toBeDefined();
-});
-
-test('should be null if not found', () => {
-  const a = { b: 'baby', c: { d: 'dad', e: { f: ['fish', 'fly'] } } };
-  expect(find(a, 'null')).toBeNull();
-});
+describe("Main Test", () => Suite('./'));
+// describe("Alt Test", () => Suite('./alt.js'));
